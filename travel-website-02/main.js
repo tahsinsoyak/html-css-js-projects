2740a24f7cb657f2d0ab8f8652be6fdbb13226f1
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-10 10:41:41 +0300

    Add files via upload

---
+++ travel-website-02/main.js
@@ -0,0 +1,14 @@
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+function videoToggle(){
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    trailer.classList.toggle('active');
+    video.currentTime=0;
+    video.pause();
+}
